02e67a73f6ed2c09f1601b0ec51dcb154de8c36c
LiamAttClarke <liam.att.clarke@gmail.com>
2014-11-26 13:56:42 -0500

    Added Rat Character, Fixed 'Wall'
    Added rat model, Limited wall-obstacle's vertical movement

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+	public float max_y;
+	public float min_y;
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, Mathf.Clamp (transform.position.y, min_y, max_y), transform.position.z);
+	}
+}
